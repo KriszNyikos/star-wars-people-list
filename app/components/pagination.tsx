bc2a13cb662af82362b41d7dabe0e90e470de0e2
KriszNyikos <krisz.nyikos@gmail.com>
2023-07-18 15:54:38 +0200

    add basic style and card component

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -10,31 +10,52 @@ export default function Pagination({
   onPageChange: Function;
 }) {
   const pagesCount = Math.ceil(count / 10);
-
   if (pagesCount === 1) return null;
-
   const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
 
   return (
     <div>
-        {
-            currentPage !== 1 ?  <Link href={`/list?page=${currentPage - 1}`} key={currentPage - 1} onClick={() => onPageChange(currentPage - 1)}>prev</Link> : null
-        }
-       
+      {
+        <Link
+          href={`/list?page=${currentPage - 1}`}
+          key={currentPage - 1}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          <button
+            disabled={currentPage === 1}
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+          >
+            prev
+          </button>
+        </Link>
+      }
+
       {pages.map((page) => {
         return (
           <Link href={`/list?page=${page}`} key={page}>
-            <button onClick={() => onPageChange(page)}>
+            <button
+              className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 ${page === currentPage &&"bg-gray-400"}` }
+              onClick={() => onPageChange(page)}
+            >
               {page}
-              {page === currentPage && " (current)"}
             </button>
           </Link>
         );
       })}
       {
-        currentPage !== pagesCount ? <Link href={`/list?page=${currentPage + 1}`} key={currentPage + 1} onClick={() => onPageChange(currentPage + 1)}>next</Link> : null
+        <Link
+          href={`/list?page=${currentPage + 1}`}
+          key={currentPage + 1}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          <button
+            disabled={currentPage === pagesCount}
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+          >
+            next
+          </button>
+        </Link>
       }
-      
     </div>
   );
 }
